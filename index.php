3eaa0450c0e0adafc94e4c279a152bf6b1a94fd4
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-19 15:43:37 +0200

    add $filmOne with new Movie();

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ class Movie
     public $title;
     public $length;
     public $country;
-    public $originalLenguage;
+    public $director;
     public $publicationYear;
 }
+
+$filmOne = new Movie();
+$filmOne->title = 'Fight Club';
+$filmOne->length = '139 min';
+$filmOne->country = 'America';
+$filmOne->director = 'David Fincher';
+$filmOne->publicationYear = '1999';
+var_dump($filmOne);
