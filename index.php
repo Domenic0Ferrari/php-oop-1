6def1a27d9e65ce697950c86dc80d0f0ae467513
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-20 10:37:06 +0200

    add style and fix foreach

--- index.php
+++ index.php
@@ -11,14 +11,18 @@ include __DIR__ . '/db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
 
 <body>
-    <div class="container">
-        <ul><?php
-            foreach ($movies as $movie) { ?>
+    <div class="container bg-success">
+        <h1 class="text-center text-primary">FILM</h1>
+        <ul class="text-bg-success p-3"><?php foreach ($movies as $movie) { ?>
                 <li>
-                    <?= $movie->title ?> - <?= $movie->length ?> - <?= $movie->country ?> - <?= $movie->director ?> - <?= $movie->publicationYear ?> - <?php foreach ($movie->genres as $genre) { ?> <?= $genre->firstGenre ?> - <?= $genre->secondGenre ?> - <?= $genre->thirdGenre ?> <?php } ?></li> <?php } ?>
+                    <?= $movie->title ?> - <?= $movie->length ?> - <?= $movie->country ?> - <?= $movie->director ?> - <?= $movie->publicationYear ?> - <?php foreach ($movie->genres as $genre) { ?> <?= $genre->firstGenre ?> - <?= $genre->secondGenre ?> - <?= $genre->thirdGenre ?> <?php } ?>
+                </li>
+            <?php } ?>
         </ul>
     </div>
 </body>
